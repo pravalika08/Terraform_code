bd0f582c7125656543cd718170a67c6cd91e7c27
pravalika08 <78397122+pravalika08@users.noreply.github.com>
2024-06-13 15:06:55 +0530

    Update main.tf

--- Pravalika_04052021/main.tf
+++ Pravalika_04052021/main.tf
@@ -108,7 +108,7 @@ resource "aws_route" "public_nat_gateway" {
     destination_cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.ngw.id
 }
-#/*------------------KET_PAIR------------------------------------*/
+#/*------------------KEY_PAIR------------------------------------*/
 resource "aws_key_pair" "ec2key" {
   key_name = "ebs"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
